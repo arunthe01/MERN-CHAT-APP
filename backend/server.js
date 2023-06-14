59f9c779ff2a179a099bdc512dc470b8423bda85
Arun emmidi <djdjdjaaa999@gmail.com>
2023-06-15 04:50:22 +0530

    done

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./Routes/userRoutes');
+const chatRoutes = require('./Routes/chatRoutes');
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Api is running');
+})
+app.use('/api/user',userRoutes);
+// app.use('/api/chat',chatRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,console.log("hello world!"));
---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { Button } from '@chakra-ui/react';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/chats' element={<Chatpage/>}/>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
